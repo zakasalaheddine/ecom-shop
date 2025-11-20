a127f0dc0b91f3d6978242cbb6171b9d63269dd8
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2025-11-20 13:47:42 +0100

    feat: convex schema and provider

---
+++ convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  categories: defineTable({
+    label: v.string(),
+    imageUrl: v.string(),
+  }),
+  types: defineTable({
+    label: v.string(),
+    imageUrl: v.string(),
+  }),
+  products: defineTable({
+    title: v.string(),
+    price: v.number(),
+    category: v.id("categories"),
+    type: v.id("types"),
+    imageUrl: v.string(),
+    listingUrl: v.string(),
+  }),
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        >
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
